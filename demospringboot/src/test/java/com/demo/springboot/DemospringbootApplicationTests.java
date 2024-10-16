a840ae29e7d758cc7de5bcbd61b4017c3face86b
Meghana S <116013635+meghu03@users.noreply.github.com>
2024-10-16 17:58:04 +0530

    Add files via upload

---
+++ demospringboot/src/test/java/com/demo/springboot/DemospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
