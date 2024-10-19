ebf20b0cf161acb45d3a21b7fd3b7a2c2c1d9b83
Meghana S <116013635+meghu03@users.noreply.github.com>
2024-10-19 13:46:56 +0530

    Delete productrestapi directory

--- productrestapi/src/main/java/com/example/springweb/repos/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springweb.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springweb.entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
